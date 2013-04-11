ec809b2b973916bd2aacd4e9a19fc68b7af41878
ArnauVich <arnauvich@gmail.com>
2013-04-11 11:31:02 +0200

    removing path variable

--- QC/Sequence_duplication.groovy
+++ QC/Sequence_duplication.groovy
@@ -1,11 +1,9 @@
-PATH_TO_PRINSEQ="~/prinseq-lite-0.20.3/prinseq-lite.pl"
-PATH_TO_GRAPH="~/prinseq-lite-0.20.3/prinseq-graphs.pl"
 
 Sequence_duplication = {
-	exec "perl $PATH_TO_PRINSEQ -fastq $input -graph_data -graph_stats da -out_good null -out_bad null"
+	exec "perl prinseq-lite -fastq $input -graph_data -graph_stats da -out_good null -out_bad null"
 }
 Graph_values = {
-	exec "perl $PATH_TO_GRAPH -i $input -png_all"
+	exec "perl prinseq-graphs -i $input -png_all"
 }
 Bpipe.run {
 	Sequence_duplication + Graph_values
