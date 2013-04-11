f26404ec8a802c268e301d4daffa169ce95036e2
ArnauVich <arnauvich@gmail.com>
2013-04-11 11:28:45 +0200

    removing path variable

--- QC/GC_content.groovy
+++ QC/GC_content.groovy
@@ -1,11 +1,9 @@
-PATH_TO_PRINSEQ="~/prinseq-lite-0.20.3/prinseq-lite.pl"
-PATH_TO_GRAPH="~/prinseq-lite-0.20.3/prinseq-graphs.pl"
 
 GC_content = {
-	exec "perl $PATH_TO_PRINSEQ -fastq $input -graph_data -graph_stats gc -out_good null -out_bad null"
+	exec "perl prinseq-lite.pl -fastq $input -graph_data -graph_stats gc -out_good null -out_bad null"
 }
 Graph_values = {
-	exec "perl $PATH_TO_GRAPH -i $input -png_all"
+	exec "perl prinseq-graphs.pl -i $input -png_all"
 }
 Bpipe.run {
 	GC_content + Graph_values
