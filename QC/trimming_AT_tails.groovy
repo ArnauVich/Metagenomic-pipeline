3c3e45f61be695c69d9df9f9ba1db6babddaeec9
ArnauVich <arnauvich@gmail.com>
2013-04-11 11:32:09 +0200

    removing path variable

--- QC/trimming_AT_tails.groovy
+++ QC/trimming_AT_tails.groovy
@@ -1,10 +1,9 @@
-PATH_TO_PRINSEQ="~/prinseq-lite-0.20.3/prinseq-lite.pl"
 TRIM_RIGHT= 5
  //Trim the minimum A/T tail at 3' end 
 TRIM_LEFT= 5
  //Trim the minimum A/T tail at 5' end
 trim_tails = {
-	exec "perl $PATH_TO_PRINSEQ -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -out_bad null -log"
+	exec "perl prinseq-lite.pl -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -out_bad null -log"
 }
 Bpipe.run {
 	trim_tails
