da1f2c38e7c2e181ea1f28956bc825415885e906
ArnauVich <arnauvich@gmail.com>
2013-04-22 09:57:11 +0200

    implement to pipeline

--- QC/artifact_filter.groovy
+++ QC/artifact_filter.groovy
@@ -1,7 +1,7 @@
 Artifact_filter= {
-	exec "fastx_artifacts_filter -Q 33 -i $input -o noartifact.fastq"
+	filter ("f"){
+	exec "fastx_artifacts_filter -Q 33 -i $input -o $output.fastq"
+	exec "rm $input"
 }
-Bpipe.run {
-	Artifact_filter
 }
 //Q 33 indicates that Sanger/Illumina is using instead of Solexa/Illumina
