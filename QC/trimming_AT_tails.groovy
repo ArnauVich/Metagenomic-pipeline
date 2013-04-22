756921bfd36e1d39e10b4a63e3abb31e68795d69
ArnauVich <arnauvich@gmail.com>
2013-04-22 10:13:12 +0200

    implement to pipeline

--- QC/trimming_AT_tails.groovy
+++ QC/trimming_AT_tails.groovy
@@ -1,10 +1,5 @@
-TRIM_RIGHT= 5
- //Trim the minimum A/T tail at 3' end 
-TRIM_LEFT= 5
- //Trim the minimum A/T tail at 5' end
-trim_tails = {
-	exec "perl prinseq-lite.pl -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -out_bad null -log"
+Trim_tails = {
+	filter ("tr") {
+	exec "perl $PATH_TO_PRINSEQ -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -log -out_bad null -out_good $output.prefix"
 }
-Bpipe.run {
-	trim_tails
-} 
+}
