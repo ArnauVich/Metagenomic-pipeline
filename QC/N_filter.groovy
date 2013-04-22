d9d0e065235dad53bc21a22d7ce89ab139d576ae
ArnauVich <arnauvich@gmail.com>
2013-04-22 09:59:12 +0200

    implement to pipeline

--- QC/N_filter.groovy
+++ QC/N_filter.groovy
@@ -1,9 +1,6 @@
-NPERC= 40
-// maxim percentage of N's bases admited, value from 0 to 100
-
 N_filter={
-	exec "perl prinseq-lite.pl -fastq $input -ns_max_p $NPERC -out_bad null"
+	filter ("Nf"){
+	exec "perl prinseq-lite.pl -fastq $input -ns_max_p $NPERC -out_bad null -out_good $output.prefix"
+	exec "rm $input"
 }
-Bpipe.run{
-	N_filter
 }
