d2bd2cc0c6d80cc2fc200236cb7e3c8ed2cc430b
ArnauVich <arnauvich@gmail.com>
2013-04-22 10:07:44 +0200

    implement to pipeline

--- QC/Sequence_duplication.groovy
+++ QC/Sequence_duplication.groovy
@@ -1,10 +1,6 @@
-
 Sequence_duplication = {
-	exec "perl prinseq-lite -fastq $input -graph_data -graph_stats da -out_good null -out_bad null"
+	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats da -out_good null -out_bad null"
 }
-Graph_values = {
-	exec "perl prinseq-graphs -i $input -png_all"
-}
-Bpipe.run {
-	Sequence_duplication + Graph_values
+Sequence_duplication_fil = {
+	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats da -out_good null -out_bad null"
 }
