882a3c873b3dcb40ec3e651f06b32409c81fdce6
ArnauVich <arnauvich@gmail.com>
2013-06-01 10:52:10 +0200

    remove perl

--- QC/Dinucleotide_odds.groovy
+++ QC/Dinucleotide_odds.groovy
@@ -1,6 +1,6 @@
 Dinucleotide_odds = {
-	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats dn -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats dn -out_good null -out_bad null "
 }
 Dinucleotide_odds_fil = {
-	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats dn -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats dn -out_good null -out_bad null "
 }
