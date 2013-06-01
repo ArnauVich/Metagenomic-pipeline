bb4db9bacbdf4f4c6ed5f49deb8ab7ba67dd3276
ArnauVich <arnauvich@gmail.com>
2013-06-01 10:54:15 +0200

    remove perl

--- QC/N_filter.groovy
+++ QC/N_filter.groovy
@@ -1,6 +1,6 @@
 N_filter={
 	filter ("Nf"){
-	exec "perl prinseq-lite.pl -fastq $input -ns_max_p $NPERC -out_bad null -out_good $output.prefix"
+	exec "prinseq-lite.pl -fastq $input -ns_max_p $NPERC -out_bad null -out_good $output.prefix"
 	exec "rm $input"
 }
 }
