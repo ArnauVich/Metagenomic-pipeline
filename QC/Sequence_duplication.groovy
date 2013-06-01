08becbeb4265c95fe4079589152abd4bdb4969d7
ArnauVich <arnauvich@gmail.com>
2013-06-01 10:55:09 +0200

    remove perl

--- QC/Sequence_duplication.groovy
+++ QC/Sequence_duplication.groovy
@@ -1,6 +1,6 @@
 Sequence_duplication = {
-	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats da -out_good null -out_bad null"
+	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats da -out_good null -out_bad null"
 }
 Sequence_duplication_fil = {
-	exec "perl prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats da -out_good null -out_bad null"
+	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats da -out_good null -out_bad null"
 }
