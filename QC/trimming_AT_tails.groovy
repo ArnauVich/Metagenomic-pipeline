f9871d4dd614a4e245e862db72ad40c05588df0d
ArnauVich <arnauvich@gmail.com>
2013-06-01 10:56:23 +0200

    remove perl

--- QC/trimming_AT_tails.groovy
+++ QC/trimming_AT_tails.groovy
@@ -1,5 +1,5 @@
 Trim_tails = {
 	filter ("tr") {
-	exec "perl $PATH_TO_PRINSEQ -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -log -out_bad null -out_good $output.prefix"
+	exec "prinseq-lite.pl -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -log -out_bad null -out_good $output.prefix"
 }
 }
