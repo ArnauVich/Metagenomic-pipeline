8847c8dd5623f8872cb4280bc772b09cfc6872a7
ArnauVich <arnauvich@gmail.com>
2013-06-18 18:45:17 +0200

    improving script

--- QC/trimming_AT_tails.groovy
+++ QC/trimming_AT_tails.groovy
@@ -1,5 +1,6 @@
 Trim_tails = {
 	filter ("tr") {
 	exec "prinseq-lite.pl -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -log -out_bad null -out_good $output.prefix"
+	exec "rm $input"
 }
 }
