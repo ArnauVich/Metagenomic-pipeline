2479552e9e9f25e9281626ce614d9ce8f15ae9c5
ArnauVich <arnauvich@gmail.com>
2013-06-18 19:19:01 +0200

    add annotation parameters

---
+++ Annotation_Parameters.groovy
@@ -0,0 +1,14 @@
+PATH_TO_FGS = "/home/arnau/FragGeneScan1.16/FragGeneScan"
+// FragGeneScans needs to indicates where is installed to work proparly
+PART= 0
+// 1 for complete genome sequences
+// 0 short sequences reads
+TRAIN= "illumina_5"
+// [complete] for complete genomic sequences or short sequence reads without sequencing error
+// [sanger_5] for Sanger sequencing reads with about 0.5% error rate
+// [sanger_10] for Sanger sequencing reads with about 1% error rate
+// [454_5] for 454 pyrosequencing reads with about 0.5% error rate
+// [454_10] for 454 pyrosequencing reads with about 1% error rate
+// [454_30] for 454 pyrosequencing reads with about 3% error rate
+// [illumina_5] for Illumina sequencing reads with about 0.5% error rate
+// [illumina_10] for Illumina sequencing reads with about 1% error rate
