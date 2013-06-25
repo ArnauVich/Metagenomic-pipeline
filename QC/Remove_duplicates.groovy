52ec606adeae2be640bc54bdfa615bc640fdb5ac
ArnauVich <arnauvich@gmail.com>
2013-06-25 17:15:15 +0200

    improve little things

--- QC/Remove_duplicates.groovy
+++ QC/Remove_duplicates.groovy
@@ -1,3 +1,5 @@
 Remove_duplicates = {
+	filter ("dup"){
 	exec " tally -i $input -o $output --nozip --with-quality -sumstat ./graphics_results/statistic_files/statistics_duplicates"
-}
+	exec "rm $input"
+}}
