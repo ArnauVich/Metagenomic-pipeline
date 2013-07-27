b8ff86c7e60ff75013ad7bc957ca4cbe956aa53a
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:12:08 +0200

    output as current directory and new feature

--- QC/artifact_filter.groovy
+++ QC/artifact_filter.groovy
@@ -1,6 +1,7 @@
 Artifact_filter= {
-	filter ("f"){
+	filter ("t"){
 	exec "fastx_artifacts_filter -Q 33 -i $input -o $output.fastq"
+	exec """cat $input | awk 'BEGIN{stage="Artifact_filter"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
