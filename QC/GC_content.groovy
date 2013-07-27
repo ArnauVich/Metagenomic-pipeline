3f313f04891ade794df8b89d64262c456cb23770
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:10:28 +0200

    output as current directory and new feature

--- QC/GC_content.groovy
+++ QC/GC_content.groovy
@@ -1,10 +1,10 @@
 GC_content = {
 
-	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/$output.gd -graph_stats gc -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/$output.gd -graph_stats gc -out_good null -out_bad null "
 }
 
 GC_content_fil = {
 
-	exec "prinseq-lite.pl -fastq $input -graph_data graphics_results/filtered_graphics/$output.gd -graph_stats gc -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/filtered_graphics/$output.gd -graph_stats gc -out_good null -out_bad null "
 }
 
