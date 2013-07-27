4d0cee30ec6f7bc6978c256c0e5ca9c1ba9ae510
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:11:00 +0200

    output as current directory and new feature

--- QC/Remove_adapters.groovy
+++ QC/Remove_adapters.groovy
@@ -1,4 +1,5 @@
 Remove_adapters = {
-	filter ("ad"){
-	exec "cutadapt $input -g $ADAPTER1 -g $ADAPTER2 -O $OVERLAP --info-file ./graphics_results/statistic_files/statistics_cutadapt -o $output -e $ERROR"
+	filter ("f"){
+	exec "cutadapt $input -g $ADAPTER1 -g $ADAPTER2 -O $OVERLAP --info-file ./statistic_files/statistics_cutadapt -o $output -e $ERROR >./statistic_files/adapters"
+	exec """cat $input | awk 'BEGIN{stage="Remove adapters"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >./statistic_files/quality_stage"""
 }}
