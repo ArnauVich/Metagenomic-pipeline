cdfec5b515125ed049acbfd3085f4fb5cd4f3be0
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:13:02 +0200

    output as current directory and new feature

--- QC/trimming_AT_tails.groovy
+++ QC/trimming_AT_tails.groovy
@@ -1,6 +1,7 @@
 Trim_tails = {
-	filter ("tr") {
+	filter ("i") {
 	exec "prinseq-lite.pl -fastq $input -trim_tail_left $TRIM_LEFT -trim_tail_right $TRIM_RIGHT -log -out_bad null -out_good $output.prefix"
+	exec """cat $input | awk 'BEGIN{stage="Trim_tails"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
