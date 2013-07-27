abfad805746aae330b3521a0ae02e94033a4a2d2
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:12:22 +0200

    output as current directory and new feature

--- QC/filter_length.groovy
+++ QC/filter_length.groovy
@@ -1,6 +1,7 @@
 Length_filter= {
-	filter ("l"){
+	filter ("r"){
 	exec "ht-filter -F length -i $input -L $LEN_CUT -o $output.prefix"
+	exec """cat $input | awk 'BEGIN{stage="Length_filter"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
