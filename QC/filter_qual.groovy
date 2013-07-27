bb0cfab7b05e4f00dd9e4d3adbf0f2f4a927818e
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:12:28 +0200

    output as current directory and new feature

--- QC/filter_qual.groovy
+++ QC/filter_qual.groovy
@@ -1,6 +1,7 @@
 Quality_filter={
-	filter ("q"){
+	filter ("e"){
 	exec "ht-filter -F quality -i $input -Q $QUAL_CUT -o $output.prefix"
+	exec """cat $input | awk 'BEGIN{stage="Quality_filter"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
