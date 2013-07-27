e4b7fa84b3c9f7fcfd9c8cece91a680badcdb859
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:10:50 +0200

    output as current directory and new feature

--- QC/N_filter.groovy
+++ QC/N_filter.groovy
@@ -1,6 +1,7 @@
 N_filter={
-	filter ("Nf"){
+	filter ("e"){
 	exec "prinseq-lite.pl -fastq $input -ns_max_p $NPERC -out_bad null -out_good $output.prefix"
+	exec """cat $input | awk 'BEGIN{stage="N_filter"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
