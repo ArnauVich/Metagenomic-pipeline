d8983d92d98348b1afb6e842fab3b34236d6bb23
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:09:50 +0200

    output as current directory

--- quality_control.groovy
+++ quality_control.groovy
@@ -1,9 +1,10 @@
 load "QC_parameters.groovy"
 Results={
-	exec "mkdir ./graphics_results/"
-	exec "mkdir ./graphics_results/filtered_graphics"
-	exec "mkdir ./graphics_results/statistic_files"
+	exec "mkdir ./QC_graphics_results/"
+	exec "mkdir ./QC_graphics_results/filtered_graphics"
+	exec "mkdir ./statistic_files"
 }
+
 
 Statistics_graph= segment {
 	Statistics + draw_statistics 
@@ -13,7 +14,7 @@ Statistics_filtered= segment {
 }
 
 Quality_graph= segment {
-	[Dinucleotide_odds + Graph_values, Sequence_duplication + Graph_values, GC_content + Graph_values]
+	[Dinucleotide_odds, Sequence_duplication, GC_content]
 }
 Filtered_graph= segment {
 	[Dinucleotide_odds_fil, Sequence_duplication_fil, GC_content_fil]
@@ -28,6 +29,9 @@ Bpipe.run{
 }
 Bpipe.run{
 	Quality_graph
+}
+Bpipe.run{
+	Graph_values
 }
 Bpipe.run {
 	Remove_adapters + Trim_tails + Ends_trimming + Artifact_filter + N_filter + Quality_filter + Length_filter + Remove_duplicates + [Filtered_graph, Statistics_graph_2_fil] 
