b91062691897f6896715106a2e26f8d2b6794fd3
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:11:53 +0200

    output as current directory and new feature

--- QC/Statistics_graph_2.groovy
+++ QC/Statistics_graph_2.groovy
@@ -1,6 +1,7 @@
 Statistics_graph_2 = {
-	exec " fastqc $input -o ./graphics_results/"
+	exec " fastqc $input -o ./QC_graphics_results/"
 }
 Statistics_graph_2_fil = {
-	exec " fastqc $input -o ./graphics_results/filtered_graphics/"
+	exec " fastqc $input -o ./QC_graphics_results/filtered_graphics/"
+	exec """cat $input | awk 'BEGIN{stage="Total after Quality control"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >./statistic_files/quality_stage"""
 }
