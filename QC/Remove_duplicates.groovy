9aee724345a668cb14918715c6d056018c9a9cd6
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:11:18 +0200

    output as current directory and new feature

--- QC/Remove_duplicates.groovy
+++ QC/Remove_duplicates.groovy
@@ -1,5 +1,6 @@
 Remove_duplicates = {
-	filter ("dup"){
-	exec " tally -i $input -o $output --nozip --with-quality -sumstat ./graphics_results/statistic_files/statistics_duplicates"
+	filter ("ed"){
+	exec " tally -i $input -o $output --nozip --with-quality -sumstat ./statistic_files/statistics_duplicates"
+	exec """cat $input | awk 'BEGIN{stage="Remove_duplicates"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }}
