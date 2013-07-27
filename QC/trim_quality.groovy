1ad5c86322925481ca57c9a713760ef55b3c6ccb
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:12:55 +0200

    output as current directory and new feature

--- QC/trim_quality.groovy
+++ QC/trim_quality.groovy
@@ -1,6 +1,7 @@
 Ends_trimming = {
-	filter ("et") {
+	filter ("l") {
 	exec "ht-trim -i $input -S $TRIM_OP -c $QUAL_CUT -o $output"
+	exec """cat $input | awk 'BEGIN{stage="Ends_trimming"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 	exec "rm $input"
 }
 }
