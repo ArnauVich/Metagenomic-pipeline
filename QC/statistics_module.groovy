feeac0689ec68b7e169a1903bddcbc8c17e841a8
ArnauVich <arnauvich@gmail.com>
2013-07-27 12:12:39 +0200

    output as current directory and new feature

--- QC/statistics_module.groovy
+++ QC/statistics_module.groovy
@@ -1,6 +1,6 @@
-OUT_DIR= "./graphics_results/"
+OUT_DIR= "./QC_graphics_results/"
  //output directory for statistics summary
-OUT_DIR_FIL= "./graphics_results/filtered_graphics/statistics_graph/"
+OUT_DIR_FIL= "./QC_graphics_results/filtered_graphics/statistics_graph/"
  //output directory for statistics summary
 Statistics = {
 	exec "ht-stat -i $input -o $OUT_DIR -l $MAX_READ_LEN "
@@ -9,7 +9,7 @@ draw_statistics = {
 	exec "ht-stat-draw.pl --dir $OUT_DIR --format png "
 }
 Statistics_fil = {
-	exec "mkdir ./graphics_results/filtered_graphics/statistics_graph/"
+	exec "mkdir ./QC_graphics_results/filtered_graphics/statistics_graph/"
 	exec "ht-stat -i $input -o $OUT_DIR_FIL -l $MAX_READ_LEN "
 }
 draw_statistics_fil = {
