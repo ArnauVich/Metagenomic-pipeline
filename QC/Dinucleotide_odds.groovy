41990a0e0dfbd35702adb7a97de51e9514b24761
ArnauVich <arnauvich@gmail.com>
2013-08-04 13:01:12 +0200

    Improving outputs

--- QC/Dinucleotide_odds.groovy
+++ QC/Dinucleotide_odds.groovy
@@ -1,6 +1,6 @@
 Dinucleotide_odds = {
-	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/$output.gd -graph_stats dn -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data $output.gd -graph_stats dn -out_good null -out_bad null "
 }
 Dinucleotide_odds_fil = {
-	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/filtered_graphics/$output.gd -graph_stats dn -out_good null -out_bad null "
+	exec "prinseq-lite.pl -fastq $input -graph_data $output.gd -graph_stats dn -out_good null -out_bad null "
 }
