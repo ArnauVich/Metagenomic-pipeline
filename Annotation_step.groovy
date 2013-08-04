091d3e07b70fb948862c97e453f6e09eb28e924c
ArnauVich <arnauvich@gmail.com>
2013-08-04 12:48:47 +0200

    Implementation of all pipeline

--- Annotation_step.groovy
+++ Annotation_step.groovy
@@ -1,32 +1,45 @@
 load "Annotation_Parameters.groovy"
 
 Create_dir ={
-	exec "mkdir Annotation" 
+	exec "mkdir ./graph" 
+	exec "mkdir ./Hits_databases"
 }
 
-Orf_predictor = "FGS_pred"
 
 FGS_pred= segment{
-	FGS + Statistics_FGS
+	FGS + Statistics_FGS + Blat_f + Select_columns + Use_sqlite
 }
 Prodigal_pred= segment{
-	Prodigal + Statistics_prodigal
+	Prodigal + Statistics_prodigal + Blat_p + Select_columns + Use_sqlite
 }
 
-
+Ribosomic= segment {
+	Blat_r + Select_columns + Use_sqlite
+}
 
 if (ORF == "FGS") {
-	Orf_predictor = FGS_pred
+	Annotation_pipe = FGS_pred
 }
 if (ORF == "Prodigal"){
-	Orf_predictor = Prodigal_pred
+	Annotation_pipe = Prodigal_pred
 }
-else { Orf_predictor = FGS_pred }
+if (ORF == "ribosomic"){
+	Annotation_pipe = Ribosomic
+}
 
-//Bpipe.run {
-//	Create_dir
-//}
 
 Bpipe.run {
-	Orf_predictor + Blat + Select_columns + Use_sqlite
+	Create_dir
+}
+Bpipe.run{
+	Annotation_pipe
+}
+//Bpipe.run {
+//	Orf_predictor + Blat + Select_columns + Use_sqlite
+//}
+Bpipe.run{
+	[Graph_hits, Graph_tax, Graph_Ontology, Graph_Clusters, Graph_ribosomic]
+}
+Bpipe.run{
+	Create_graphic
 }
