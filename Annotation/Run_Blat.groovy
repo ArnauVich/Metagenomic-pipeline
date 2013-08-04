d14e2d87c8ec9e23d82911e038d2c3b3659c0890
ArnauVich <arnauvich@gmail.com>
2013-08-04 12:46:43 +0200

    3 blat stages

--- Annotation/Run_Blat.groovy
+++ Annotation/Run_Blat.groovy
@@ -1,5 +1,15 @@
-Blat = {
-	from ("*.faa"){
+Blat_p = {
+	from ("faa"){
 	exec "blat $PATH_TO_Md5nr $input -prot -out=blast8 -minScore=$SCORE -minIdentity=$MIN_ID $output"
+	exec "select_id.sh $input "
 }
+}
+Blat_f ={
+	exec "blat $PATH_TO_Md5nr ${input}.faa -prot -out=blast8 -minScore=$SCORE -minIdentity=$MIN_ID $output"
+	exec "select_id.sh $input "
+}
+
+Blat_r = {
+	exec "blat $PATH_TO_md5rna $input -t=dna -q=dna -out=blast8 -minScore=$SCORE -minIdentity=$MIN_ID $output"
+	exec "select_id.sh $input "
 }
