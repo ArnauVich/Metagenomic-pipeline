1bac2eac1190365e25378f2bdad30e593e2b4c0d
ArnauVich <arnauvich@gmail.com>
2013-08-04 13:10:02 +0200

    top ten and count stats

---
+++ scripts/Hits.sh
@@ -0,0 +1,2 @@
+cat $1  | awk -F "\t" '{print $7}'| sort | uniq -c | awk -F " " '{array[$0]=$1; sum+=$1} END { for (i in array) printf "%s\t%.2f\n",i,array[i]/sum*100}' | sort -nr -k1 | awk '{sub(/^ */,""); print}' | awk '{sub(/  /,"\t"); print}' > "${1}_graph" 
+cat $1  | awk -F "\t" '{print $1}'| sort | uniq -c | sort -nr | awk -F " " '{print $1"\t"$2}' | head -10 > "${1}_top_ten"
