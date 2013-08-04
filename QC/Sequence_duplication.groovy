654d96171c3ac83c39f63242c5ac936365fdcda7
ArnauVich <arnauvich@gmail.com>
2013-08-04 13:02:56 +0200

    Improving outputs

--- QC/Sequence_duplication.groovy
+++ QC/Sequence_duplication.groovy
@@ -1,6 +1,6 @@
 Sequence_duplication = {
-	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/$output.gd -graph_stats da -out_good null -out_bad null"
+	exec "prinseq-lite.pl -fastq $input -graph_data $output.gd -graph_stats da -out_good null -out_bad null"
 }
 Sequence_duplication_fil = {
-	exec "prinseq-lite.pl -fastq $input -graph_data QC_graphics_results/filtered_graphics/$output.gd -graph_stats da -out_good null -out_bad null"
+	exec "prinseq-lite.pl -fastq $input -graph_data $output.gd -graph_stats da -out_good null -out_bad null"
 }
