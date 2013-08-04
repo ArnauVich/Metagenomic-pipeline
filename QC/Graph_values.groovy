69b5b1b9a1b06ab2c837fa9c3fe55fa8a044dc5f
ArnauVich <arnauvich@gmail.com>
2013-08-04 13:01:44 +0200

    Improving outputs

--- QC/Graph_values.groovy
+++ QC/Graph_values.groovy
@@ -1,9 +1,32 @@
-PATH_TO_GD="./QC_graphics_results/*.gd"
-PATH_TO_GD_FIL="./QC_graphics_results/filtered_graphics/*.gd"
-Graph_values = {
-	exec "prinseq-graphs.pl -i $PATH_TO_GD -png_all "
+PATH_TO_GD="./QC_graphics_results/"
+PATH_TO_GD_FIL="./QC_graphics_results/filtered_graphics/"
+Graph_values_dinucleotide = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD/dinucleotide -png_all "
+	exec "rm $input"
+}
+Graph_values_gc = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD/gc -png_all "
+	exec "rm $input"
+}
+Graph_values_duplicates = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD/duplicates -png_all "
+	exec "rm $input"
+}
+Graph_values_fil_dinucleotide = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD_FIL/dinucleotide -png_all "
+	exec "rm $input"
+}
+Graph_values_fil_gc = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD_FIL/gc -png_all "
+	exec "rm $input"
+}
+Graph_values_fil_duplicates = {
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD_FIL/duplicates -png_all "
+	exec "rm $input"
 }
 Graph_values_fil = {
-	exec "prinseq-graphs.pl -i $PATH_TO_GD_FIL -png_all "
+	from ("*.gd")
+	exec "prinseq-graphs.pl -i $input -o $PATH_TO_GD_FIL/output -png_all "
+	exec "rm $input"
 
 }
