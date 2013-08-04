04859d1f518414f90142a1d6cb205d2e1e3546a7
ArnauVich <arnauvich@gmail.com>
2013-08-04 13:03:26 +0200

    Improving FASTQC

--- QC/Statistics_graph_2.groovy
+++ QC/Statistics_graph_2.groovy
@@ -1,7 +1,9 @@
 Statistics_graph_2 = {
 	exec " fastqc $input -o ./QC_graphics_results/"
+	exec "mv ./QC_graphics_results/*fastqc ./QC_graphics_results/fastqc"
 }
 Statistics_graph_2_fil = {
 	exec " fastqc $input -o ./QC_graphics_results/filtered_graphics/"
-	exec """cat $input | awk 'BEGIN{stage="Total after Quality control"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >./statistic_files/quality_stage"""
+	exec "mv ./QC_graphics_results/filtered_graphics/*fastqc ./QC_graphics_results/filtered_graphics/fastqc"
+	exec """cat $input | awk 'BEGIN{stage="Total after Quality control"}{if(NR%4==2){count++}} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage"""
 }
