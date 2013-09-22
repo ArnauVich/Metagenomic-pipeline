76b08b1ff7a25c3e36a65a696a860f27d64cc714
ArnauVich <arnauvich@gmail.com>
2013-09-22 22:48:02 +0200

    add filtering results

---
+++ Annotation/Tables.groovy
@@ -0,0 +1,4 @@
+Tables = {
+	exec " cat ./statistic_files/statistics_ORF | head -50 > ./statistic_files/statistics_ORF_selected"
+	exec " cat ./statistic_files/cluster_sum | head -50 > ./statistic_files/cluster_sum_selected"
+}
