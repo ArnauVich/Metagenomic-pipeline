6d90d599dc7173efbdf2df35169f09a349662c96
ArnauVich <arnauvich@gmail.com>
2013-09-22 22:50:57 +0200

    ribosomal info

---
+++ scripts/select_id_ribo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cat $1 | awk '/^>/{print $1}' | awk '{sub(/>/,"",$0); print}'| sort | uniq > ./Hits_databases/Ids
+cat ./Hits_databases/Ids | awk 'BEGIN{stage=" Number of clusters"}{count=NR} END {printf "%s\t %d\n",stage,count}' >>./statistic_files/quality_stage
