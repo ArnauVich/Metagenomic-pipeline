b81554992e36f52d177c5525778234c46597a62c
ArnauVich <arnauvich@gmail.com>
2013-09-22 22:46:30 +0200

    minor change

--- scripts/select_id.sh
+++ scripts/select_id.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat $1 | awk '/^>/{print $1}' | awk '{sub(/>/,"",$0); print}'| sort | uniq > ./Hits_databases/Ids
-cat ./Hits_databases/Ids | awk 'BEGIN{stage="Clustered sequences"}{count=NR} END {printf "%s\\t %d\\n",stage,count}' >>./statistic_files/quality_stage
+cat ./Hits_databases/Ids | awk 'BEGIN{stage=" Predicted Genes"}{count=NR} END {printf "%s\t %d\n",stage,count}' >>./statistic_files/quality_stage
